a2d51011ff03de433508731acb2827c262124903
Kouei <hungjui11@gmail.com>
2024-02-26 17:27:16 +0800

    port specified

--- add_api/app.py
+++ add_api/app.py
@@ -14,5 +14,5 @@ def add_numbers():
         return {'error': 'Missing parameters'}, 400  # Directly returning dictionary
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
 
