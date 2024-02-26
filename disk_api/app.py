dc597e2624a513635401cf8c91ac8439babe57fd
Kouei <hungjui11@gmail.com>
2024-02-26 16:36:31 +0800

    wrong app.py

--- disk_api/app.py
+++ disk_api/app.py
@@ -1,16 +1,17 @@
-import requests
+from flask import Flask, jsonify
+import psutil
 
-# Make a GET request to the API endpoint
-response = requests.get('http://127.0.0.1:5000/api/disk-usage')
+app = Flask(__name__)
 
-# Check if the request was successful (status code 200)
-if response.status_code == 200:
-    # Print the disk usage information returned by the API
-    data = response.json()
-    print("Total disk space:", data['total'])
-    print("Used disk space:", data['used'])
-    print("Free disk space:", data['free'])
-else:
-    # Print an error message if the request was not successful
-    print("Error:", response.status_code)
+@app.route('/api/disk-usage', methods=['GET'])
+def get_disk_usage():
+    disk_usage = psutil.disk_usage('/')
+    return jsonify({
+        'total': disk_usage.total,
+        'used': disk_usage.used,
+        'free': disk_usage.free
+    })
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
 
