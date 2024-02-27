149de4920cd7f699fb6d9412e6493c460965f352
Kouei <hungjui11@gmail.com>
2024-02-27 14:26:57 +0800

    port set

--- ntfy_api/app.py
+++ ntfy_api/app.py
@@ -33,5 +33,5 @@ def send_notification():
         return jsonify({'success': False, 'error': 'Message not provided'})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
 
