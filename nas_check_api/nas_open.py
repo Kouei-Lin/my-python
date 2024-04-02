9b3e44638478eacee60735373730ef91029b9ba7
Kouei <hungjui11@gmail.com>
2024-04-02 17:59:53 +0800

    ssh

--- nas_check_api/nas_open.py
+++
@@ -1,9 +0,0 @@
-import webbrowser
-
-# List of URLs you want to open
-urls = ['https://www.google.com', 'https://en.wikipedia.org']
-
-# Open each URL in the default browser
-for url in urls:
-    webbrowser.open(url)
-
