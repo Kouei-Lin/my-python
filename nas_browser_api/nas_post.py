80cab591d7d31601f21ea75dfcef42e3d92314cd
Kouei <hungjui11@gmail.com>
2024-04-12 17:12:36 +0800

    rm

--- nas_browser_api/nas_post.py
+++ nas_browser_api/nas_post.py
@@ -7,7 +7,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from dotenv import load_dotenv
-from datetime import datetime
 
 
 class SynType1:
