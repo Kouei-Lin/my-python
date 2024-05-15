a84b17a71a415bc06115102587eb38dbf1c21e0c
Kouei <hungjui11@gmail.com>
2024-05-15 11:41:44 +0800

    loop

--- email_imap/main.py
+++ email_imap/main.py
@@ -86,7 +86,7 @@ class EmailDataSaver:
     
     def save_to_csv(self, csv_filename):
         with open(csv_filename, 'w', newline='', encoding='utf-8') as csvfile:
-            fieldnames = ['Subject', 'Date', 'Size', 'Read', 'Transferred', 'Duration']
+            fieldnames = ['Subject', 'Date', 'Size', 'Read', 'Transferred', 'Duration', 'Status']
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
             writer.writeheader()
             writer.writerows(self.emails_content)
@@ -101,20 +101,22 @@ class EmailDataSaver:
                     size TEXT,
                     read TEXT,
                     transferred TEXT,
-                    duration TEXT
+                    duration TEXT,
+                    status TEXT
                 )
             ''')
             for email in self.emails_content:
                 cursor.execute('''
-                    INSERT INTO emails (subject, date, size, read, transferred, duration)
-                    VALUES (?, ?, ?, ?, ?, ?)
+                    INSERT INTO emails (subject, date, size, read, transferred, duration, status)
+                    VALUES (?, ?, ?, ?, ?, ?, ?)
                 ''', (
                     email['Subject'],
                     email['Date'],
                     email['Size'],
                     email['Read'],
                     email['Transferred'],
-                    email['Duration']
+                    email['Duration'],
+                    email['Status']
                 ))
 
 def convert_to_gb_or_mb(value_with_unit):
@@ -131,50 +133,58 @@ def main():
     imap_server_url = os.getenv('IMAP_SERVER_URL')
     imap_username = os.getenv('IMAP_USERNAME')
     imap_password = os.getenv('IMAP_PASSWORD')
-    mailbox_folder = os.getenv('MAIL_FOLDER')
+    mailbox_folders = {
+        'Veeam/Failed': 'Failed',
+        'Veeam/Replication': 'Retry',
+        'Veeam/Success': 'Success',
+        'Veeam/Warning': 'Warning'
+    }
     
     client = EmailIMAPClient(imap_server_url, imap_username, imap_password)
     client.login()
     
-    email_ids = client.fetch_emails(mailbox_folder)
-    
     extractor = EmailContentExtractor()
     emails_content = []
     
-    # Fetch emails and parse them
-    for email_uid in email_ids:
-        try:
-            print(f"Fetching email with UID: {email_uid}...")
-            status, email_data = client.imap_server.fetch(email_uid, '(RFC822)')
-            if status != 'OK':
-                print(f"Failed to fetch email with UID {email_uid}.")
-                continue
-            
-            subject, date, body = extractor.parse_email_content(email_data[0][1])
-            start_time, end_time, size, read, transferred, duration = extractor.extract_information_from_body(body)
-            
-            subject_value = extractor.determine_subject(subject)
-            size_value = convert_to_gb_or_mb(size)
-            read_value = convert_to_gb_or_mb(read)
-            transferred_value = convert_to_gb_or_mb(transferred)
-            
-            email_info = {
-                'Subject': subject_value,
-                'Date': date,
-                'Size': size_value,
-                'Read': read_value,
-                'Transferred': transferred_value,
-                'Duration': duration
-            }
-            emails_content.append(email_info)
-        except Exception as e:
-            print(f"Error processing email with UID {email_uid}: {e}")
+    # Loop through each mailbox folder
+    for folder, status in mailbox_folders.items():
+        email_ids = client.fetch_emails(folder)
+        
+        # Fetch emails and parse them
+        for email_uid in email_ids:
+            try:
+                print(f"Fetching email with UID: {email_uid} from folder {folder}...")
+                status, email_data = client.imap_server.fetch(email_uid, '(RFC822)')
+                if status != 'OK':
+                    print(f"Failed to fetch email with UID {email_uid}.")
+                    continue
+                
+                subject, date, body = extractor.parse_email_content(email_data[0][1])
+                start_time, end_time, size, read, transferred, duration = extractor.extract_information_from_body(body)
+                
+                subject_value = extractor.determine_subject(subject)
+                size_value = convert_to_gb_or_mb(size)
+                read_value = convert_to_gb_or_mb(read)
+                transferred_value = convert_to_gb_or_mb(transferred)
+                
+                email_info = {
+                    'Subject': subject_value,
+                    'Date': date,
+                    'Size': size_value,
+                    'Read': read_value,
+                    'Transferred': transferred_value,
+                    'Duration': duration,
+                    'Status': status
+                }
+                emails_content.append(email_info)
+            except Exception as e:
+                print(f"Error processing email with UID {email_uid}: {e}")
     
     # Close the connection to the IMAP server
     client.logout()
     
     # Save parsed email content to SQLite database
-    db_filename = f"{mailbox_folder.replace('/', '_')}.db"
+    db_filename = 'emails_data.db'
     data_saver = EmailDataSaver(emails_content)
     data_saver.save_to_sqlite(db_filename)
     print(f"Email content saved to {db_filename}")
