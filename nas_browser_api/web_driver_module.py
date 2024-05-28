0101f10e38463a9c6db2cccec8966cb995c9aa36
Kouei <hungjui11@gmail.com>
2024-05-28 17:48:39 +0800

    driver

--- nas_browser_api/web_driver_module.py
+++ nas_browser_api/web_driver_module.py
@@ -1,24 +1,14 @@
-import os
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 
 class WebDriverManager:
-    def __init__(self, grid_url=None):
-        if grid_url:
-            self.driver = self.setup_grid_driver(grid_url)
-        else:
-            self.driver = self.setup_local_driver()
-
-    def setup_grid_driver(self, grid_url):
+    def __init__(self, grid_url="http://127.0.0.1:4444/wd/hub"):
         ff_options = Options()
         ff_options.add_argument("--headless")
-        return webdriver.Remote(
+        self.driver = webdriver.Remote(
             command_executor=grid_url,
             options=ff_options
         )
-
-    def setup_local_driver(self):
-        return webdriver.Firefox()
 
     def get_driver(self):
         return self.driver
@@ -26,5 +16,4 @@ class WebDriverManager:
     def quit_driver(self):
         if self.driver:
             self.driver.quit()
-            self.driver = None
 
