903c13311b55d44bf8c9a96c861563837e9e7699
Kouei <hungjui11@gmail.com>
2024-06-19 17:51:22 +0800

    fix:back

--- pywebio/reboot/reboot_app.py
+++ pywebio/reboot/reboot_app.py
@@ -1,23 +1,19 @@
 from pywebio.input import *
 from pywebio.output import *
 from pywebio.session import *
+from pywebio.pin import *
 from pywebio import start_server
 from pywebio_battery import *
-import time
 
 def main():
     put_markdown('# Reboot System')
-    
+   
     def reboot_callback(btn):
+        run_js('window.location.reload()')
         if btn == 'Reboot':
-            put_text('Server is rebooting...')  # Display rebooting message
-            for i in range(5, 0, -1):
-                put_text(f"Countdown: {i} seconds")
-                time.sleep(1)
-            put_text('Server is offline. Please wait for reboot...')
+            run_shell("sudo reboot")
     
     put_buttons(['Reboot'], onclick=reboot_callback)
     
-if _name_ == '__main__':
+if name == '__main__':
     start_server(main, debug=True, port=8080)  # Use port 8080 or another available port
-
