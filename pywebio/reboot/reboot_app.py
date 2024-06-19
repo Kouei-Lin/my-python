2b51046e11a48c9afa94eae22ac1f7cf17dd2e8c
Kouei <hungjui11@gmail.com>
2024-06-19 17:47:28 +0800

    feat: additional msg

--- pywebio/reboot/reboot_app.py
+++ pywebio/reboot/reboot_app.py
@@ -1,16 +1,20 @@
 from pywebio.input import *
 from pywebio.output import *
 from pywebio.session import *
-from pywebio.pin import *
 from pywebio import start_server
 from pywebio_battery import *
+import time
 
 def main():
     put_markdown('# Reboot System')
-   
+    
     def reboot_callback(btn):
         if btn == 'Reboot':
-            run_shell("sudo reboot")
+            put_text('Server is rebooting...')  # Display rebooting message
+            for i in range(5, 0, -1):
+                put_text(f"Countdown: {i} seconds")
+                time.sleep(1)
+            put_text('Server is offline. Please wait for reboot...')
     
     put_buttons(['Reboot'], onclick=reboot_callback)
     
