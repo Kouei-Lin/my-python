5d5f888c78ba160d3b06c4ed4792c30ab2eed3eb
Kouei <hungjui11@gmail.com>
2024-06-20 17:05:54 +0800

    reloading

--- reboot_app/pywebio_reboot/reboot_app.py
+++ reboot_app/pywebio_reboot/reboot_app.py
@@ -12,11 +12,28 @@ def main():
         if btn == 'Reboot':
             user_confirmed = confirm("Are you sure you want to reboot?")
             if user_confirmed:
+                put_html('<div id="countdown">Reloading in <span id="countdown-span">60</span> seconds...</div>')
+                js_code = """
+                <script>
+                var seconds = 60;
+                var countdown = setInterval(function() {
+                    seconds--;
+                    document.getElementById('countdown-span').textContent = seconds;
+                    if (seconds <= 0) {
+                        clearInterval(countdown);
+                        // Add a delay before rebooting
+                        setTimeout(function() {
+                            location.reload();
+                        }, 5000); // 5000 milliseconds (5 seconds) delay
+                    }
+                }, 1000);
+                </script>
+                """
+                put_html(js_code)
                 run_shell("sudo reboot")
     
     put_buttons(['Reboot'], onclick=reboot_callback)
     
-if __name__ == '__main__':
+if _name_ == '__main__':
     start_server(main, debug=True, port=18080)
-
 
